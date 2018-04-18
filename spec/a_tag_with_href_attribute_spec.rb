d491c8774d3b2be7e7707c07cda0c2f919b0f8c6
Brandon Brasson <rocksong622@gmail.com>
2018-04-18 23:51:18 +0000

    automatically backed up by learn

--- spec/a_tag_with_href_attribute_spec.rb
+++ spec/a_tag_with_href_attribute_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "An <a> tag with an 'href' attribute" do
     expect(a_tag.text).to eq('Flatiron School')
   end
 
-  it "has the href attribute of 'https://flatironschool.com'" do
+  puts it "has the href attribute of 'https://flatironschool.com'" do
     a_tag = parsed_html.search('a').first
 
     expect(a_tag.attr('href')).to eq('https://flatironschool.com')
